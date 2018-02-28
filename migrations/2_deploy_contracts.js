80c08f888a35ed2a28db0eaaf46020b1e37a735c
Lucas Della Bella <dellabella.lucas@gmail.com>
2018-02-28 18:33:41 -0500

    Deployment of Congress.sol working

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ var Congress = artifacts.require("./Congress.sol");
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, Congress);
-  deployer.deploy(Congress);
+  deployer.deploy(Congress, 0, 0, 0);
 };
