ba35ebab7720c5fe113697fbb9bdb7e8756c794d
Lucas Della Bella <dellabella.lucas@gmail.com>
2018-03-13 16:34:34 -0400

    Switch to explicit ganache-cli in .travis.yml

--- scripts/ganache-run.sh
+++ scripts/ganache-run.sh
@@ -3,10 +3,6 @@
 cd .logs
 
 # Possibly node.*ganache will not pick up ganache-cli
-if (nc -z localhost 8545); then
-    echo "Using existing ganache instance on port $(ps -fade | grep -e 'node.*ganache' | head -n 1 | awk '{print $2}')"
-else
-    echo -n "Starting ganache instance on port $port "
-    ganache-cli "$port" > ganache.log 2>&1 & echo $! > ganache.pid
-    echo $(cat ganache.pid)
-fi
+echo -n "Starting ganache instance on port $port "
+ganache-cli "$port" > ganache.log 2>&1 & echo $! > ganache.pid
+echo $(cat ganache.pid)
