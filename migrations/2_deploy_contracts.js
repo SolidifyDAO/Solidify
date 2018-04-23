bdde05115eedaf56072bbb535641bba8a47e38bd
Lucas Della Bella <dellabella.lucas@gmail.com>
2018-04-22 22:37:59 -0400

    attempting to deploy DAO from server

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-var ConvertLib = artifacts.require('./ConvertLib.sol');
-var DAO = artifacts.require('./DAO.sol');
-
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, DAO);
-  //deployer.deploy(DAO);
-};
--- server/server.py
+++ server/server.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+from pprint import pformat
 import os
+import subprocess
+import json
 
 app = Flask(__name__)
 
@@ -14,8 +18,22 @@ def hello():
 @app.route("/createDAO", methods=['POST'])
 def createDAO():
   daoData = request.get_json()
+  timestamp = str(datetime.now()).replace(" ", ":")
+  dao_filepath = "".join(["../usergenerated/dao/dao-", timestamp, '.json'])
+  with open(dao_filepath, 'w') as f:
+    f.write(json.dumps(daoData, indent=4))
+  run_deployment_script('2_createDAO.js', [dao_filepath])
   print(daoData)
   return "nice."
+
+def run_deployment_script(script_name, args_list):
+  print(os.getcwd())
+  old_path = "".join(["../notmigrations/", script_name])
+  new_path = "".join(["../migrations/", script_name])
+  os.rename(old_path, new_path)
+  output = subprocess.check_output(" ".join(["truffle migrate"] + args_list), shell=True)
+  print(output)
+  os.rename(new_path, old_path)
 
 @app.route("/tempdao")
 def tempdao():
@@ -65,6 +83,6 @@ def tempdao():
   return render_template('dao.html', dao=dao)
 
 # should use 'flask run' instead
-#if __name__ == '__main__':
-  #port = int(os.environ.get('PORT', 5000))
-  #app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
