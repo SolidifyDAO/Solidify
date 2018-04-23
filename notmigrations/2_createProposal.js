11d0ae23274e8218b8acd1b7427dc1fb6cd393b6
Lucas Della Bella <dellabella.lucas@gmail.com>
2018-04-23 05:09:02 -0400

    Fix incorrect createProposal contents.

--- notmigrations/2_createProposal.js
+++ notmigrations/2_createProposal.js
@@ -1,33 +1,76 @@
-var DAO = artifacts.require('./DAO.sol');
+var Proposal = artifacts.require('./Proposal.sol');
+var AddMember = artifacts.require('./AddMember.sol');
+var AddRole = artifacts.require('./AddRole.sol');
 
 function clArgs(index) {
   return process.argv[index + 4]
 }
 
-module.exports = function(callback) {
+module.exports = function(deployer) {
   // pull in the json file specific to the dao we want to create
-  newDao = require(clArgs(0));
+  newProposal = require(clArgs(0));
+  let choiceName = null;
+  let choiceAddress = null;
+  let daoAddress = newProposal['dao_address'];
+  let votingEndTime = newProposal['voting_length'];
 
-  daoName = newDao['name'];
-  distScheme = newDao['voting'];
-  roleNames = newDao['roles'].map(function(role) { return role['name'] });
-  roleVotes = newDao['roles'].map(function(role) { return role['votes'] });
-  memberAddrs = newDao['members'].map(function(member) { return member['address'] });
-  memberNames = newDao['members'].map(function(member) { return member['name'] });
-  memberRoles = newDao['members'].map(function(member) { return member['role'] });
+  // Hack to check proposal type until joe passes down an explicit 'type' param.
+  newProposal['type'] = newProposal.hasOwnProperty('votes') ? 'AddRole' : 'HirePerson';
 
-  DAO.new(
-    daoName,
-    distScheme,
-    roleNames,
-    roleVotes,
-    memberAddrs,
-    memberNames,
-    memberRoles
+  if (newProposal['type'] == 'AddRole') {
+    // deploy AddRole proposal
+    roleName = newProposal['name']
+    roleVotingPower = newProposal['votes']
+
+    // TODO: UNUSED
+    roleDescription = newProposal['description']
+
+    deployer.deploy(
+      AddRole,
+      roleVotingPower,
+      roleName
+    ).then(function(instance) {
+      console.log(instance.address);
+    }).catch(function(err) {
+      console.log('ERROR!');
+      console.log(err);
+    });
+
+    choiceName = "Create The '" + roleName + "' Role"
+    choiceAddress = (await AddRole.deployed()).address
+  } else if (newProposal['type'] == 'HirePerson') {
+    // deploy AddMember proposal
+    memberAddress = newProposal['address']
+    memberName = newProposal['name']
+    memberRole = newProposal['role']
+
+    deployer.deploy(
+      AddMember,
+      memberAddress,
+      memberName,
+      memberRole
+    ).then(function(instance) {
+      console.log(instance.address);
+    }).catch(function(err) {
+      console.log('ERROR!');
+      console.log(err);
+    });
+
+    choiceName = "Hire " + memberName
+    choiceAddress = (await AddMember.deployed()).address
+  }
+
+  // create proposal
+  deployer.deploy(
+    Proposal,
+    [choiceName],
+    [choiceAddress],
+    daoAddress
   ).then(function(instance) {
     console.log(instance.address);
   }).catch(function(err) {
     console.log('ERROR!');
     console.log(err);
   });
+
 };
